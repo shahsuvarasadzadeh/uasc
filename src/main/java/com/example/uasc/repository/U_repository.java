b0f89ccc06e8b29e9a532cf6670712e1e27ee02f
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-03-31 14:35:32 +0400

    Repository Interface

---
+++ src/main/java/com/example/uasc/repository/U_repository.java
@@ -0,0 +1,7 @@
+package com.example.uasc.repository;
+
+import com.example.uasc.entity.U_entity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface U_repository extends JpaRepository<U_entity,Long> {
+}
