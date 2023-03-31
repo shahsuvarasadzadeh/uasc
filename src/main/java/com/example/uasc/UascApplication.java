600abd0aba4f78c203e8afc9b965ff0da8c2fd24
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-03-31 14:33:25 +0400

    uasc start

---
+++ src/main/java/com/example/uasc/UascApplication.java
@@ -0,0 +1,13 @@
+package com.example.uasc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UascApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UascApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/uasc/UascApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uasc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UascApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
