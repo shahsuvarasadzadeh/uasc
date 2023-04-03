e928f124d1c0b130ff6ede6c7434f1b54e91cfbd
shahsuvarasadzadeh <106305870+shahsuvarasadzadeh@users.noreply.github.com>
2023-04-03 16:49:45 +0400

    exception message

---
+++ src/main/java/com/example/uasc/GenericResponse.java
@@ -0,0 +1,10 @@
+package com.example.uasc;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+@Getter
+@Setter
+@AllArgsConstructor
+public class GenericResponse {
+    private String message;
+}
